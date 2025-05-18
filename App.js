7e89b7b79c7479e4f96179fdbf757e0fc5c05a1c
Michael Mburu <ichangaimichael@yahoo.com>
2025-05-18 16:25:06 +0300

    Switched to Safeareaview context

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView, Button, Linking } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
